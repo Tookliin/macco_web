e4ba6f4f65120bf02d719f5b8011ab37a9ab42f8
Tookliin <maccoskuni@gmail.com>
2020-03-28 18:41:47 +0200

    Commited nothing  Changes to be committed:

--- base/url.py
+++ base/url.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = "main"
 urlpatterns = [
-    path('', views.home)
+    path('', views.home),
+    path('about', views.about, name="about"),
+    path('contact', views.contact, name="contact"),
+    path('experience', views.experience, name="experience")
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 # Create your views here.
 def home(request):
     return render(request, 'main/home.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
+def contact(request):
+    return render(request, 'main/contact.html')
+
+
+def experience(request):
+    return render(request, 'main/experience.html')
